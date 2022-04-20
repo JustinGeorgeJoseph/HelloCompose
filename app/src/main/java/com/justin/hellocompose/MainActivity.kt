140d2d901a51ee5fb7c154f99178c4dc40eb4fcb
justingeorge <justin@entri.me>
2022-04-20 23:48:34 +0530

    Implemented a sample widget with details

--- app/src/main/java/com/justin/hellocompose/MainActivity.kt
+++ app/src/main/java/com/justin/hellocompose/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.justin.hellocompose.ui.theme.HelloComposeTheme
 
 data class Book(val author:String, val name: String)
@@ -26,8 +31,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun showMessageCard(message:Book){
-    Text(text = message.author)
-    Text(text = message.name)
+    Row (modifier = Modifier.padding(all = 8.dp)){
+        Image(painter = painterResource(id = R.drawable.justin),
+            contentDescription = "Test image",
+        modifier = Modifier.size(40.dp).clip(CircleShape))
+        Column(modifier = Modifier.padding(start = 8.dp)){
+            Text(text = message.author)
+            Text(text = message.name,modifier = Modifier.padding(top = 4.dp))
+        }
+    }
+
 }
 
 @Preview
