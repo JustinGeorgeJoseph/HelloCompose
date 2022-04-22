0292fffc3758d3f2b958b407768c574891dd8787
justingeorge <justin@entri.me>
2022-04-22 21:46:49 +0530

    Implement list view

--- app/src/main/java/com/justin/hellocompose/MainActivity.kt
+++ app/src/main/java/com/justin/hellocompose/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -20,16 +22,65 @@ import androidx.compose.ui.unit.dp
 import com.justin.hellocompose.ui.theme.HelloComposeTheme
 
 data class Book(val author:String, val name: String)
+data class Messages(val author:String, val name: String)
 
 class MainActivity : ComponentActivity() {
+    val messageList = arrayListOf(
+        Messages("Justin","HI"),
+        Messages("Adam","Hi"),
+        Messages("Justin","Nice to meet you!"),
+        Messages("Adam","Same Here"),
+        Messages("Justin","Ente daivame ne evidanu!"),
+        Messages("Adam","Onnum ponam mister, njan evide indu, ne evide anu"),
+        Messages("Justin","Ah Angane anengil okay....."),
+        Messages("Justin","Ente daivame ne evidanu!"),
+        Messages("Adam","Onnum ponam mister, njan evide indu, ne evide anu"),
+        Messages("Justin","Ah Angane anengil okay....."),
+        Messages("Adam","Appo sry bye bye"),
+        Messages("Justin","Bye"),
+        Messages("Justin","Take care")
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             HelloComposeTheme {
-                showMessageCard(message = Book("Justin","Audacity of hope"))
+                showConversationList(messages = messageList)
             }
         }
     }
+}
+
+@Composable
+fun showConversationList(messages: List<Messages>) {
+    LazyColumn{
+        items(messages){ item ->
+            showMessageCard(item)
+        }
+    }
+}
+
+@Composable
+fun showMessageCard(message:Messages){
+    Row (modifier = Modifier.padding(all = 8.dp),){
+        Image(painter = painterResource(id = R.drawable.justin),
+            contentDescription = "Test image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+        Column(modifier = Modifier.padding(start = 8.dp)) {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(text = message.name,modifier = Modifier.padding(all = 8.dp))
+            }
+        }
+    }
+
 }
 
 @Composable
@@ -56,8 +107,25 @@ fun showMessageCard(message:Book){
 
 }
 
-@Preview
 @Composable
 fun MessageCard(){
     showMessageCard(message = Book("Justin","Audacity of hope"))
+}
+
+@Preview
+@Composable
+fun previewConversionList(){
+    val messageList = arrayListOf(
+        Messages("Justin","HI"),
+        Messages("Adam","Hi"),
+        Messages("Justin","Nice to meet you!"),
+        Messages("Adam","Same Here"),
+        Messages("Justin","Ente daivame ne evidanu!"),
+        Messages("Adam","Onnum ponam mister, njan evide indu, ne evide anu"),
+        Messages("Justin","Ah Angane anengil okay....."),
+        Messages("Adam","Appo sry bye bye"),
+        Messages("Justin","Bye"),
+        Messages("Justin","Take care")
+    )
+    showConversationList(messages = messageList)
 }
